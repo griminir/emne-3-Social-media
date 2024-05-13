694e49b6dbf7e0509e3749c0d69dd1ef0f2f10b0
griminir <viktor.elliot.king@gmail.com>
2024-05-13 11:09:14 +0200

    Add project files.

---
+++ Social-media/User.cs
@@ -0,0 +1,37 @@
+﻿namespace Social_media
+{
+    public class User()
+    {
+        internal string Name { get; set; }
+        internal int Age { get; set; }
+        internal string AboutMe { get; set; }
+        internal string Hobbies { get; set; }
+
+        public User(string name, int age, string aboutMe, string hobbies)
+        {
+            Name = name;
+            Age = age;
+            AboutMe = aboutMe;
+            Hobbies = hobbies;
+        }
+
+        public User(string name)
+        {
+            Name = name;
+        }
+
+        //public void MakeAboutMe()
+        //{
+        //    Console.Write("Tell us something about you.");
+        //    var userinput = Console.ReadLine();
+        //    AboutMe = userinput;
+        //}
+
+        //public void MakeHobbies()
+        //{
+        //    Console.Write("What hobbies do you have?");
+        //    var userinput = Console.ReadLine();
+        //    Hobbies = userinput;
+        //}
+    }
+}
