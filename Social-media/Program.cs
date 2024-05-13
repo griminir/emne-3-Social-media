e883e7bb5c5d65b09dfe2ba62b2581cf91ace618
griminir <viktor.elliot.king@gmail.com>
2024-05-13 14:13:24 +0200

    took some time but got it done

--- Social-media/Program.cs
+++ Social-media/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Social_media;
+
 Console.WriteLine("Social media!");
+
+
+User Kåre = new("kåre", 32, "chill dude just chilling", "cycling");
+User Oskar  = new("oskar", 19, "just here because i need to contact my mom sometimes", "gaming");
+User Kine  = new("kine", 28, "omg new website I need to join", "horseriding");
+
+while (true)
+{
+    Kåre.ShowMenu();
+}
--- Social-media/User.cs
+++ Social-media/User.cs
@@ -1,37 +1,108 @@
-﻿namespace Social_media
-{
-    public class User()
-    {
-        internal string Name { get; set; }
-        internal int Age { get; set; }
-        internal string AboutMe { get; set; }
-        internal string Hobbies { get; set; }
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Channels;
 
-        public User(string name, int age, string aboutMe, string hobbies)
+namespace Social_media
+{
+    internal class User
+    {
+        internal string Name;
+        internal int Age;
+        internal string AboutMe;
+        internal string Hobbies;
+        internal List<User?> FriendList = new List<User?>();
+        internal static List<User> Everyone = new List<User>();
+
+        public User(string name, int age, string about, string hobbies)
         {
             Name = name;
             Age = age;
-            AboutMe = aboutMe;
+            AboutMe = about;
             Hobbies = hobbies;
+            Everyone.Add(this);
         }
 
-        public User(string name)
+        internal void AddFriend()
         {
-            Name = name;
+            Console.WriteLine("Who do you wish to add?");
+            var friendinput = Console.ReadLine();
+            var newfriend = Everyone.Find(f=> f.Name == friendinput);
+            FriendList.Add(newfriend);
         }
 
-        //public void MakeAboutMe()
-        //{
-        //    Console.Write("Tell us something about you.");
-        //    var userinput = Console.ReadLine();
-        //    AboutMe = userinput;
-        //}
+        internal void RemoveFriend()
+        {
+            Console.WriteLine("who do you wish to remove?");
+            var friendinput = Console.ReadLine();
+            var enemy = FriendList.FirstOrDefault(f => f.Name == friendinput);
+            FriendList.Remove(enemy);
+        }
 
-        //public void MakeHobbies()
-        //{
-        //    Console.Write("What hobbies do you have?");
-        //    var userinput = Console.ReadLine();
-        //    Hobbies = userinput;
-        //}
+        internal void PrintFriendList()
+        {
+            foreach(User? friend in FriendList)
+            {
+                Console.WriteLine($"my friend: {friend.Name}");
+            }
+        }
+
+        internal void ChooseFriendPage()
+        {
+            var count = 0;
+            foreach (User? friend in FriendList)
+            {
+                Console.WriteLine($"{count} show {friend.Name} page");
+                count++;
+            }
+
+            var userinput = Console.ReadLine();
+            ShowProfile(userinput);
+        }
+
+        internal void ShowProfile(string choice)
+        {
+            int num;
+            int.TryParse(choice, out num);
+            var Friend = FriendList[num];
+            Console.WriteLine("friend's profile");
+            Console.WriteLine($"{Friend.Name}");
+            Console.WriteLine($"{Friend.Age}");
+            Console.WriteLine($"{Friend.AboutMe}");
+            Console.WriteLine($"{Friend.Hobbies}");
+            Console.WriteLine();
+        }
+
+        public void ShowMenu()
+        {
+            string[] menu = new[] { "1. add friend", "2. remove friend", "3. show friendlist", "4. show a friends page"};
+            Console.WriteLine($"Hey {Name}");
+            Console.WriteLine($"what would you like to do?");
+            for (int i = 0; i < menu.Length; i++)
+            {
+                Console.WriteLine(menu[i]);
+            }
+            Console.WriteLine();
+
+            var userinput = Console.ReadLine();
+
+            switch (userinput)
+            {
+                case "1":
+                    AddFriend();
+                    break;
+                case "2":
+                    RemoveFriend();
+                    break;
+                case "3":
+                    PrintFriendList();
+                    break;
+                case "4":
+                    ChooseFriendPage();
+                    break;
+                default:
+                    Console.WriteLine("logged out");
+                    Environment.Exit(0);
+                    break;
+            }
+        }
     }
 }
